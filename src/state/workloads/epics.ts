055fff59be5d0cecdc81566920af4eca4df8a2a0
unknown <garima.mann>
2021-06-26 16:39:47 +0530

    Add create workload functionality

--- src/state/workloads/epics.ts
+++ src/state/workloads/epics.ts
@@ -1,25 +1,40 @@
 import { combineEpics, Epic } from 'redux-observable';
-import { filter, map, tap, ignoreElements } from 'rxjs/operators';
+import { filter, map, tap, mergeMap} from 'rxjs/operators';
 import { isActionOf } from 'typesafe-actions';
 
 import { RootAction, RootState } from '../reducer';
 import * as workloadsActions from './actions';
+import { WorkloadService } from '././services';
+
+const service = new WorkloadService()
 
 
 type AppEpic = Epic<RootAction, RootAction, RootState>;
 
-const logWorkloadSubmissions: AppEpic = (action$, state$) => (
+
+const createWorkload: AppEpic = (action$, state$) => (
   action$.pipe(
     filter(isActionOf(workloadsActions.submit)),
     map(action => action.payload),
     tap((payload) => console.log('Workload submitted', payload)),
-    ignoreElements(),
+    mergeMap(async (payload) => {
+      const res = await service.create(payload)
+      return workloadsActions.created(res) 
+    }
+    ),
+    //ignoreElements(),
   )
 );
 
 
+
+
+
+
 export const epics = combineEpics(
-  logWorkloadSubmissions,
+  createWorkload,
+
+ 
 );
 
 export default epics;
